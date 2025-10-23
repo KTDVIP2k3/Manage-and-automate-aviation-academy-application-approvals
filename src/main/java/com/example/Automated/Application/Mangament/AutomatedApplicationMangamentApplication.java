838df1f7e89e8afc8529b3add473724cddc1d350
KTDVIP2k3 <Khanhtranduy996@gmail.com>
2025-10-24 00:26:40 +0700

    init

---
+++ src/main/java/com/example/Automated/Application/Mangament/AutomatedApplicationMangamentApplication.java
@@ -0,0 +1,13 @@
+package com.example.Automated.Application.Mangament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedApplicationMangamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedApplicationMangamentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Automated/Application/Mangament/AutomatedApplicationMangamentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Automated.Application.Mangament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedApplicationMangamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
